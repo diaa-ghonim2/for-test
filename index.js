54397448c95ef0b76770a17353ad649c300fdac4
Diaa Ghonim <deiaa_x@yahoo.com>
2023-02-22 22:19:41 +0200

    console

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4528e74cd522d1d0ec616af0d7ad2156eb7e27ab74c077326ec9dedbb02f665f
-size 94
+oid sha256:08447905b09510ff68c2c73dede70fc29f79b9d987433e693277d3cec432c3f0
+size 116
